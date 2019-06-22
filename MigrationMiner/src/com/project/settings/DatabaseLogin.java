23000699ad099f22a85f4b6ac17a9ef8be346a8b
hussien alrubaye <hussien89aa@yahoo.com>
2019-06-21 20:15:50 -0400

    fix bug and add output

--- MigrationMiner/src/com/project/settings/DatabaseLogin.java
+++ MigrationMiner/src/com/project/settings/DatabaseLogin.java
@@ -2,6 +2,6 @@ package com.project.settings;
 
 public class DatabaseLogin {
 	public static String url="jdbc:mysql://localhost:3306/MigrationMiner";
-	public static String username="Add database uer name";
-	public static String password="add database password";
+	public static String username="add user name here";
+	public static String password="add password here";
 }
--- MigrationMiner/src/com/project/settings/GithubLogin.java
+++ MigrationMiner/src/com/project/settings/GithubLogin.java
@@ -2,7 +2,7 @@ package com.project.settings;
 
 public class GithubLogin {
 	
-	public static String  userName="add github user name"; 
-	public static String  Password="add github password"; 
+	public static String  userName="Add user name here"; 
+	public static String  Password="add password here"; 
 
 }
