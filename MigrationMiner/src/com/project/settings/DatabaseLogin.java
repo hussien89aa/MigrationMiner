1c2ab91971aa9ad00a45b0dce314e8438cd7d828
hussien alrubaye <hussien89aa@yahoo.com>
2019-07-18 20:23:12 -0400

    security issue fix

--- MigrationMiner/src/com/project/settings/DatabaseLogin.java
+++ MigrationMiner/src/com/project/settings/DatabaseLogin.java
@@ -2,6 +2,6 @@ package com.project.settings;
 
 public class DatabaseLogin {
 	public static String url="jdbc:mysql://localhost:3306/MigrationMiner";
-	public static String username="root";
-	public static String password="12345";
+	public static String username="add user name here";
+	public static String password="add password here";
 }
